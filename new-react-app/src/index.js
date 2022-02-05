f2c393e7aa3aae5f47297eb894bf3538f2db3f96
Anna Atkinson <atkinsonsoftware@gmail.com>
2022-02-05 13:13:31 -0800

    Handle clicks of individual squares at the board level

--- new-react-app/src/index.js
+++ new-react-app/src/index.js
@@ -7,9 +7,9 @@ class Square extends React.Component {
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       >
-        {this.state.value}
+        {this.props.value}
       </button>
     );
   }
@@ -23,8 +23,19 @@ class Board extends React.Component {
     };
   }
 
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={this.state.square[i]} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
